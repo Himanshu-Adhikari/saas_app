c4391070b04c08eb63e07bac7d583beb21d0687f
Himanshu Adhikari <adhikarihimanshu393@gmail.com>
2025-09-28 11:58:54 +0530

    Clerk and Supabase Connection established

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image";
 import Navitems from "./Navitems";
-// import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 // import NavItems from "@/components/NavItems";
 
 const Navbar = () => {
@@ -17,9 +17,16 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
-            <div className="flex items-center gap-8">
-                 <Navitems/>
-                <p>Signin</p>
+           <div className="flex items-center gap-8">
+                <Navitems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
     </nav>
   )
